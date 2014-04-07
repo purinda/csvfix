28639ce3614dc16df571943ed6315176855dde3c
Purinda Gunasekara <purinda@gmail.com>
2014-04-07 21:52:40 +1000

    * Analytics Js

--- www/app/views/layouts/master.blade.php
+++ www/app/views/layouts/master.blade.php
@@ -176,10 +176,8 @@
 </body>
 
 
-</html>
-
 <!-- Google Analytics -->
-<script>
+<script type="text/javascript">
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -189,3 +187,5 @@
   ga('send', 'pageview');
 
 </script>
+
+</html>
